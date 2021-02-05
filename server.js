dcdd295806c05eb367559410be5fbc1a760f2a95
Marco <rootslowed@gmail.com>
2021-02-05 11:00:42 -0300

    mongodb connected using mongoose

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import exphbs from 'express-handlebars';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+try {
+	await mongoose.connect('mongodb://localhost/urlShortener', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+} catch (error) {
+	handleError(error); 
+}
 
 app.get('/', (req, res) => {
 	res.render('home');

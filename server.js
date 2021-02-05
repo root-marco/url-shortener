767958d9d9896134e23f6e0b2d235755d2b3747b
Marco <rootslowed@gmail.com>
2021-02-05 10:19:14 -0300

    express handlebars initial setup

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import exphbs from 'express-handlebars';
 
 const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+	res.render('home');
+});
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log('server running');

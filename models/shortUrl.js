52ca09c82a5a95df0c2269c14f8e8ac4c9c7351d
Marco <rootslowed@gmail.com>
2021-02-09 13:59:00 -0300

    all objects implemented on short url schema

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -3,4 +3,19 @@ import shortId from 'shortid';;
 
 const shortUrlSchema = new mongoose.Schema({
 
+	full: {
+		type: String,
+		required: true,
+	},
+	short: {
+		type: String,
+		required: true,
+		default: shortId.generate,
+	},
+	clicks: {
+		type: Number,
+		required: true,
+		default: 0,
+	},
+
 });

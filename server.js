f0a58ec42d01b63fe0720037dbf07cc60d9f1baa
Marco <rootslowed@gmail.com>
2021-02-09 16:01:23 -0300

    app get to list all short urls and app post to create short urls

--- server.js
+++ server.js
@@ -2,22 +2,37 @@ import express from 'express';
 import exphbs from 'express-handlebars';
 import mongoose from 'mongoose';
 
+import ShortUrl from './models/shortUrl.js';
+
 const app = express();
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+app.use(express.urlencoded({
+	extended: false
+}));
 
 try {
-	await mongoose.connect('mongodb://localhost/urlShortener', {
+	await mongoose.connect('mongodb://localhost/url-shortener', {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	});
 } catch (error) {
-	console.log(error); 
+	console.log(error);
 }
 
-app.get('/', (req, res) => {
-	res.render('home');
+app.get('/', async (req, res) => {
+	const find = await ShortUrl.find().lean();
+	res.render('home', {
+		shortUrls: find,
+	});
+});
+
+app.post('/shortUrls', async (req, res) => {
+	await ShortUrl.create({
+		full: req.body.fullUrl,
+	});
+	res.redirect('/');
 });
 
 app.listen(process.env.PORT || 3000, () => {

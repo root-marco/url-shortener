1c3e7a39315abd579ced772000cb243da3aab468
slowed <rootslowed@gmail.com>
2021-05-27 17:58:42 -0300

    initial files

--- server.js
+++ server.js
@@ -1,55 +1,24 @@
-import express from 'express';
-import exphbs from 'express-handlebars';
-import mongoose from 'mongoose';
-
-import ShortUrl from './models/shortUrl.js';
-
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+// Basic Configuration
+const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({
-	extended: false
-}));
+app.use(cors());
 
-try {
-	await mongoose.connect('mongodb://localhost/url-shortener', {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
-} catch (error) {
-	console.log(error);
-}
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', async (req, res) => {
-	const find = await ShortUrl.find().sort({
-		_id: -1
-	}).lean();
-	res.render('home', {
-		shortUrls: find,
-	});
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/shortUrls', async (req, res) => {
-	await ShortUrl.create({
-		full: req.body.fullUrl,
-	});
-	res.redirect('/');
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-app.get('/:shortUrl', async (req, res) => {
-	const findOne = await ShortUrl.findOne({
-		short: req.params.shortUrl,
-	});
-	if (findOne == null) return res.sendStatus(404);
-
-	findOne.clicks++;
-	findOne.save();
-
-	res.redirect(findOne.full);
-});
-
-app.listen(process.env.PORT || 3000, () => {
-	console.log('server running');
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });

a80b80b963200e4f9c009e4f1e586ee8e2d6fb61
slowed <rootslowed@gmail.com>
2021-05-27 18:46:59 -0300

    src folder created

---
+++ src/models/Url.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema({
+	
+	original_url: {
+		type: String,
+		required: true,
+	},
+
+	short_url: {
+		type: String,
+		required: true,
+	},
+
+});
+
+const URL = mongoose.model("URL", urlSchema);

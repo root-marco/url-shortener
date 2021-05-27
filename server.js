03afe8203066611fa197adc4041eb3cb765fe423
slowed <rootslowed@gmail.com>
2021-05-27 18:37:59 -0300

    mongodb atlas database connected

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv"; dotenv.config();
+
+// APP
 const app = express();
-
-// Basic Configuration
-const port = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3000;
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+// MONGOOSE
+try {
+  mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.info("database connected");
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
+
+// ROUTES
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/hello', function (req, res) {
+  res.json({
+    greeting: 'hello API'
+  });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+// LISTEN
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
 });

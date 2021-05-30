242725ae5dd5e056dbd7403e6f6d7f96a12e5299
slowed <rootslowed@gmail.com>
2021-05-30 15:31:26 -0300

    create new url and get the original

---
+++ src/controllers/api.js
@@ -0,0 +1,65 @@
+import URL from "../models/Url.js";
+
+export async function shortUrlPost(req, res) {
+
+	const originalURL = req.body.url;
+	const validURL = /^(http|https):\/\/[^ "]+$/;
+
+	if (validURL.test(originalURL)) {
+		try {
+			const findURL = await URL.findOne({
+				original_url: originalURL,
+			});
+			if (findURL) {
+				res.json({
+					original_url: findURL.original_url,
+					short_url: findURL.short_url,
+				});
+			} else {
+				const new_url = new URL({
+					original_url: originalURL,
+					short_url: "",
+				});
+				new_url.short_url = new_url._id.toString().slice(-6);
+				await new_url.save();
+				res.json({
+					original_url: new_url.original_url,
+					short_url: new_url.short_url,
+				});
+			}
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({
+				message: "server error",
+			});
+		}
+	} else {
+		res.json({
+			error: "invalid URL",
+		});
+	}
+
+}
+
+export async function shortUrlGet(req, res) {
+
+	try {
+		const url = await URL.findOne({
+			short_url: req.params.short_url,
+		});
+		res.redirect(url.original_url);
+	} catch (error) {
+		res.json({
+			message: "Inexistent URL"
+		});
+	}
+
+}
+
+export async function helloGet(req, res) {
+
+	res.json({
+		greeting: "hello api",
+	});
+
+}
